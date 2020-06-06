5384aa9881ca54e5343b5dee871230f038132a18
AmarnathraoSulake <amarnath.sulake@leoforce.local>
2020-06-06 17:37:54 +0530

    :white_check_mark:

--- test/Components/FNPProduct.test.js
+++ test/Components/FNPProduct.test.js
@@ -22,10 +22,24 @@ it('should not render fnpProduct component as product is {} object ', () => {
   const product = {};
 
   const wrapper = shallow(<FNPProduct product={product} />);
-  expect(wrapper.exists('parent-grid-panel')).toBeFalsy();
+  expect(wrapper.find('.parent-grid-panel')).toBeTruthy();
 });
 
 it('should not render fnpProduct component as product is undefined object ', () => {
   const wrapper = shallow(<FNPProduct />);
-  expect(wrapper.exists('parent-grid-panel')).toBeFalsy();
+  expect(wrapper.exists('.parent-grid-panel')).toBeFalsy();
+});
+it('should render bestseller', () => {
+  const product = {
+    sku: 'CAK1274',
+    title: 'Crunchy Kit Kat Cake',
+    sellingPrice: 999,
+    ratingCount: 4.3,
+    reviewCount: 88,
+    imgSrc: 'https://m-i7.fnp.com/images/pr/m/crunchy-kit-kat-cake.jpg',
+    landingPage: 'https://m.fnp.com/gift/crunchy-kit-kat-cake',
+    bestSeller: true,
+  };
+  const wrapper = shallow(<FNPProduct product={product} />);
+  expect(wrapper.find('.best-seller-label')).toBeTruthy();
 });
